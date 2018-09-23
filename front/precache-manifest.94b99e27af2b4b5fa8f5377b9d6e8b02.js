99af21c989804a58cf8c662b8ced54e2e25d75fd
李晓芳 <lixiaofang@lixiaofangdeMacBook-Air.local>
2018-09-23 18:00:17 +0800

    checkin

---
+++ front/precache-manifest.94b99e27af2b4b5fa8f5377b9d6e8b02.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "41203ca0f9b99bf6b3b3",
+    "url": "/js/chunk-vendors.35c2e700.js"
+  },
+  {
+    "revision": "428716cdc29385ac62c6",
+    "url": "/js/chunk-2d2250a1.45e2e875.js"
+  },
+  {
+    "revision": "1d6ed71a8d474ff7901e",
+    "url": "/js/app.23ccf624.js"
+  },
+  {
+    "revision": "8c06e42163bcab8fbe6af3d4b765dab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e76351ee03dba08a1fdfef3a734c84e5",
+    "url": "/img/scatter.e76351ee.png"
+  },
+  {
+    "revision": "d6c103c308ac13b9bcc7f10af05a1ea8",
+    "url": "/img/ironman.d6c103c3.png"
+  },
+  {
+    "revision": "41203ca0f9b99bf6b3b3",
+    "url": "/css/chunk-vendors.79a40762.css"
+  },
+  {
+    "revision": "1d6ed71a8d474ff7901e",
+    "url": "/css/app.875c03c6.css"
+  }
+];
